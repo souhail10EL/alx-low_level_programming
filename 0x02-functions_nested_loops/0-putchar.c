9c1c56baa1b8132621f4ddccfe43e6989a308f91
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-03-21 17:22:25 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Descrioption: 'print putchar'
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#ifndef main_h
+#define main_h
+
+int _putchar(char);
+
+
+
+
+
+
+
+#endif
