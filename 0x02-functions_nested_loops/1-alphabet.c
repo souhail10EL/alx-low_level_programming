a9207d0131e590453a1915195e790476a9882213
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-03-21 18:10:22 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check code
+ * Description: 'print alphabet'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	return (0);
+}
