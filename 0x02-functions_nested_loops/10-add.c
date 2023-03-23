df1c4c7bd698fb0c0c7679d05f473fd1b449be15
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-03-22 22:55:32 -0700

    return integer num

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - add two int numbers
+ * @a: parameter 0
+ * @s: parameter 1
+ * Return: sum
+ */
+int add(int a, int s)
+{
+	int sum;
+
+	sum = a + s;
+	return (sum);
+}
