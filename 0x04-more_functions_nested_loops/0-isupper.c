750e99561532e95e6c49177666e09e2987571a6d
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-03-26 20:39:13 -0700

    task 0 more function

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- * main - a simple function
  * _isupper - fuction to ckeck for upperletter
  * @c: parameter to ckeck if it is upper
  * Return: 1 if it is upper otherwise 0
@@ -8,7 +7,7 @@
 
 int _isupper(int c)
 {
-	if (c >= 65 &&c <= 90)
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
