cd8a9984d8171a0866e936c770669cad6c63455e
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-03-30 19:21:39 -0700

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,7 +16,7 @@ char *_strncat(char *dest, char *src, int n)
 		destle++;
 	for (i = 0; src[i] != '\0'; i++)
 		srcle++;
-	for (i = 0; i < n; i++)
+	for (i = 0; src[i] && i < n; i++)
 		dest[destle + i] = src[i];
 	return (dest);
 }
