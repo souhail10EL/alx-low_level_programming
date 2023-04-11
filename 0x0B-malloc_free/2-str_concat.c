a7d3c77b1bd9f144539192353ebe407a1cbda202
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-04-11 15:09:42 -0700

    concecanets two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,9 +25,9 @@ char *str_concat(char *s1, char *s2)
 	char *qw;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "";
 
 	size0 = _strlen(s1);
 	size1 = _strlen(s2);
