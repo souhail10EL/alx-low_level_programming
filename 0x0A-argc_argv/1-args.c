43720f97c86e3a4aeba66798185cc5c5e4c37b0c
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-04-15 11:03:42 -0700

    print number of arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - function simple
+ * @argc: argument
+ * @argv: second argument
+ * Return: 0 if success
+ */
+int main(int argc, const char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
