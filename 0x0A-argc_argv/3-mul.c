e5265232e4946a3343c2efbe620e267cb454324d
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-04-15 15:03:08 -0700

    multiple 2 num

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 /**
  * main - function in c
  * @argc: argument
  * @argv: string
  * Return: 0 if success
  */
-int main(int argc, char *argv[])
+int main(int argc, const char *argv[])
 {
+	int n1;
+	int n2;
 	int res;
 
-	if (argc == 3)
+	if (argc != 3)
 	{
-		res = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", res);
-	}
-	else
-	{
-		printf("error\n");
+		printf("Error\n");
 		return (1);
 	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	res = n1 * n2;
+	printf("%d\n", res);
 	return (0);
 }
