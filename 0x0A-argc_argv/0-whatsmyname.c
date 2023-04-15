d435ba2953fdd67b5cb2133737f817aa60f67bae
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-04-15 10:54:04 -0700

    print argv[0]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - function to printf
+ * @argc: argument
+ * @argv: another argument
+ * Return: 0 if success
+ */
+int main(int argc, const char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int main(int argc, char const *argv[]);
+#endif
