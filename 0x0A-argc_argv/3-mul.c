bcc7bbf63e204810730db1ede757c8600cb5c3f9
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-04-15 11:29:29 -0700

    print mult 2 num

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - function in c
+ * @argc: argument
+ * @argv: string
+ * Return: 0 if success
+ */
+int main(int argc, const char *argv[])
+{
+	int n1;
+	int n2;
+	int res;
+
+	if (argc != 3)
+	{
+		printf("error\n");
+		return (1);
+	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	res = n1 * n2;
+	printf("%d\n", res);
+	return (0);
+}
