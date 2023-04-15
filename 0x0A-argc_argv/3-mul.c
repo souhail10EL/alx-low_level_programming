79f18b554ce08b9c2af350f140a92eee596736f2
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-04-15 11:36:38 -0700

    multiple two num

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,14 +11,17 @@ int main(int argc, const char *argv[])
 	int n2;
 	int res;
 
-	if (argc != 3)
+	if (argc == 3)
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		res = n1 * n2;
+		printf("%d\n", res);
+	}
+	else
 	{
 		printf("error\n");
 		return (1);
 	}
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[2]);
-	res = n1 * n2;
-	printf("%d\n", res);
 	return (0);
 }
