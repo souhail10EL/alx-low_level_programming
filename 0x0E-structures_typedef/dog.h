e3df0c975ea86263c54fb6bc6b43467b53af4dae
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-04-17 16:21:32 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure alias to dog
+ * @name: pounter to dog s name
+ * @age: age of the dog
+ * @owner: pointer to dog s owner
+ *
+ * Description: information about the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
