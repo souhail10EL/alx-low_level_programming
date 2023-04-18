ba0a158d6f295897ab3362f11b99d92b94d6adc3
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-04-18 09:06:56 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -70,5 +70,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->name = _strcpy(new_dog->name, name);
 	new_dog->age = age;
 	new_dog->owner = _strcpy(new_dog->owner, owner);
+
 	return (new_dog);
 }
