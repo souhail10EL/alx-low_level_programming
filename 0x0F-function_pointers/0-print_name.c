c6a31ee91164f535b64272d994ad387553de37cc
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-04-19 16:54:41 -0700

    function pointer call print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,10 @@
 #include "function_pointers.h"
+/**
+ * print_name - function that prints name
+ * @name: pointer to name
+ * @f: function pointer
+ * Return : name
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
