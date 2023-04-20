b6299aaf0e51bf172849d73797ee6dc95edad453
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-04-19 17:17:28 -0700

    execute function

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 #include <stdlib.h>
+#include <stddef.h>
 
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
