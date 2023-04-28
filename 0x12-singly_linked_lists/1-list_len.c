e734670f2a918fd195cde430ceb5536a6c51be70
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-04-28 00:03:07 -0700

    length of node

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * list_len - prints number of elements of list
+ * @h: pointer to the header of node
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t rahsab = 0;
+
+	while (h != NULL)
+	{
+		rahsab++;
+		h = h->next;
+	}
+	return (rahsab);
+}
