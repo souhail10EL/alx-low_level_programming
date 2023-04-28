6b1b47b2161dba18d7587ca958885f3156ad95fd
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-04-28 11:51:41 -0700

    print befor main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * print - function to print before main
+ *
+ * Return: void
+ */
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
