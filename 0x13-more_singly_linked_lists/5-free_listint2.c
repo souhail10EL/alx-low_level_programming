0c8a0df7024d11eb563e492945bed316413adaeb
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-05-02 15:03:32 -0700

    freeing list and empty it

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,12 +6,12 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *cur_node, *nx_node;
+	listint_t *cur_node = *head;
+	listint_t *nx_node;
 
 	if (*head == NULL || head == NULL)
 		return;
 
-	cur_node = *head;
 	while (cur_node != NULL)
 	{
 		nx_node = cur_node->next;
