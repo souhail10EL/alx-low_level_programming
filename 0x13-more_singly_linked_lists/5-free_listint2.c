ecda4483499c990a1c93650b89098058720d925a
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-05-02 14:47:18 -0700

    freeing list and empty

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint2 - freeing list with empty list
+ * @head: double pointer to the head
+ * Return: null
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *cur_node, nx_node;
+
+	if (*head == NULL || head == NULL)
+		return;
+
+	cur_node = *head;
+	while (cur_node != NULL)
+	{
+		nx_node = cur_node->next;
+		free(cur_node);
+		cur_node = nx_node;
+	}
+	*head = NULL;
+}
