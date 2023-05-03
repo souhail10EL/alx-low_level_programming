88022b7dcf83b9d74618ffd8029cb59012603f2b
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-05-02 20:00:05 -0700

    delete node index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	return (1);
 	}
 
-	prev = NULL;
+	qber = NULL;
 	for (q = 0; q < index && rakhou != NULL; q++)
 	{
 	qber = rakhou;
